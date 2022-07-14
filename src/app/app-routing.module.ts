e597f1427767a717eb763c08aa5d9759d14778cc
Augusto Cesar <acs-03@hotmail.com>
2022-07-14 18:13:27 -0300

    Router implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookDetailComponent } from './pages/book-detail/book-detail.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'book/:id', component: BookDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
-import { Breakpoints, BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { Category } from 'src/app/shared/enums/category';
+import { IBook } from 'src/app/shared/models/book.model';
 
 @Component({
   selector: 'app-home',
@@ -11,24 +13,35 @@ export class HomeComponent implements OnInit {
   arrowRight = faArrowRight;
   arrowLeft = faArrowLeft;
   isChecked = false;
+  book!: IBook;
 
-  constructor(
-    private breakpointObserver: BreakpointObserver
-    ) {
-    this.breakpointObserver.observe([
-      "(max-width: 991px)"
-    ]).subscribe((result: BreakpointState) => {
-      if (result.matches) {
-        this.isChecked = false;
-      } else {
-        this.isChecked = true;
-      }
-    });
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver
+      .observe(['(max-width: 991px)'])
+      .subscribe((result: BreakpointState) => {
+        if (result.matches) {
+          this.isChecked = false;
+        } else {
+          this.isChecked = true;
+        }
+      });
+    this.book = {
+      id: 1,
+      name: 'Agile: Desenvolvimento de software com entregas frequentes e foco no valor de negócio',
+      price: 49.9,
+      category: Category.methodology,
+      imageUrl: '/assets/images/agile.jpg',
+      author: 'André Faria Gomes',
+      publisher: 'Casa do Código (16 abril 2014)',
+      numberOfPages: 214,
+      description:
+        'As diversas metodologias ágeis que formam o tão falado "Agile" são hoje uma das maneiras mais eficientes de guiar um projeto do ínicio ao fim, sem complicações e mantendo o tempo inteiro o foco na entrega de valor para o cliente. Nesse livro, André Faria Gomes, renomado coach e líder de equipes, explica como os diferentes sabores de Agile podem fazer um projeto de sucesso. Aprenda como o Kanban, XP e Scrum podem ser usados em conjunto e onde cada um dos métodos se complementa, além de conhecer dicas para adotar as metodologias no seu dia a dia de trabalho.',
+    };
   }
 
   ngOnInit(): void {}
 
-  checkValue(e: any){
+  checkValue(e: any) {
     this.isChecked = e.target.checked;
   }
 }
