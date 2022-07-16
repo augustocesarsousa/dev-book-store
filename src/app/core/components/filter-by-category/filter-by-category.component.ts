1e6f4b09b1234b892b244dadbc83e047f56d71b2
Augusto Cesar <acs-03@hotmail.com>
2022-07-16 11:32:32 -0300

    Filter book by category implemented

--- src/app/core/components/filter-by-category/filter-by-category.component.ts
+++ src/app/core/components/filter-by-category/filter-by-category.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/shared/enums/category';
+import { BookService } from '../../services/book-service.service';
 
 @Component({
   selector: 'app-filter-by-category',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter-by-category.component.css']
 })
 export class FilterByCategoryComponent implements OnInit {
+  category!: Category;
+  categoryList: Category[] = [];
+  categories = Category;
 
-  constructor() { }
+  constructor(private bootService: BookService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  filter(category: string, event: any){
+    this.category = (<any>Category)[category];
+    if(event.target.checked){
+      this.addFilter(this.category);
+    }else{
+      this.removeFilter(this.category);
+    }
+    this.bootService.filterByCategory(this.categoryList);
+  }
+
+  addFilter(category: Category): void{
+    this.categoryList.push(category);
+  }
+
+  removeFilter(category: Category): void{
+    this.categoryList = this.categoryList.filter(item => item !== category);
   }
 
 }
--- src/app/core/services/book-service.service.ts
+++ src/app/core/services/book-service.service.ts
@@ -2,12 +2,14 @@ import { Injectable, OnInit } from '@angular/core';
 import { bookList } from 'src/app/shared/mocks/books-list.mock';
 import { IBook } from 'src/app/shared/models/book.model';
 import { Observable, Subject } from 'rxjs';
+import { Category } from 'src/app/shared/enums/category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
   private bookList: Subject<IBook[]> = new Subject();
+  private bookListFiltered: IBook[] = [];
 
   constructor() {}
 
@@ -25,5 +27,18 @@ export class BookService {
 
   filterByName(name: string): void {
     this.bookList.next(bookList.filter((book: IBook) => book.name.toLowerCase().indexOf(name.toLowerCase()) > -1));
+  }
+
+  filterByCategory(categoryList: Category[]): void{
+    this.bookListFiltered = [];
+    if(categoryList.length > 0){
+      categoryList.map((category: Category) => {bookList
+        .filter((book: IBook) => book.category === category)
+        .map((book: IBook) => this.bookListFiltered.push(book))
+      });
+      this.bookList.next(this.bookListFiltered);
+    }else{
+      this.bookList.next(bookList);
+    }
   }
 }
